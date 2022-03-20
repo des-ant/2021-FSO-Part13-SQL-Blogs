97db85393f2675fc123eed6c679e2fa7431c6636
des-ant <57375593+des-ant@users.noreply.github.com>
2022-03-20 11:56:59 +1100

    Create a blogs table using CLI and add two blogs to the database. Save the SQL-commands in commands.sql file. -ex13.2

---
+++ commands.sql
@@ -0,0 +1,11 @@
+CREATE TABLE blogs (
+  id SERIAL PRIMARY KEY,
+  author text,
+  url text NOT NULL,
+  title text NOT NULL,
+  likes integer DEFAULT 0
+);
+
+INSERT INTO blogs (author, url, title, likes) VALUES
+  ('Edsger W. Dijkstra', 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', 'Go To Statement Considered Harmful', 5),
+  ('Michael Chan', 'https://reactpatterns.com/', 'React patterns', 7);
