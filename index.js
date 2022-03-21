44d55d0438998340b1c8c18409c703572e8b5573
des-ant <57375593+des-ant@users.noreply.github.com>
2022-03-21 22:05:10 +1100

    Change the Blogs application into a web application

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, QueryTypes } = require('sequelize')
+const express = require('express')
+const app = express()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -8,16 +10,13 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
       rejectUnauthorized: false
     }
   },
+});
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+  res.json(blogs)
 })
-
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-}
-
-main()
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
