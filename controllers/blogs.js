98abd51448290b2fbfd3da483df23aac40e7e8d7
des-ant <57375593+des-ant@users.noreply.github.com>
2022-03-23 22:52:03 +1100

    Change the structure of the blog application to match the notes application example. -ex13.5

---
+++ controllers/blogs.js
@@ -0,0 +1,43 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  console.log(JSON.stringify(blogs, null, 2))
+  res.json(blogs)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    console.log(req.blog.toJSON())
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    console.log(req.blog.toJSON())
+    await req.blog.destroy()
+  }
+  res.status(204).end()
+})
+
+module.exports = router
