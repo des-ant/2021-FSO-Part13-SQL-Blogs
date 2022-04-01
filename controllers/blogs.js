6a836a1d53781f2d605ee4935f42e4ce2bee5a16
des-ant <57375593+des-ant@users.noreply.github.com>
2022-04-01 23:13:31 +1100

    Implement filtering by keyword in the application for the route returning all blogs. -ex13.13

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,25 @@
 const router = require('express').Router()
+const { Op } = require('sequelize')
 
 const { Blog, User } = require('../models')
 const { tokenExtractor } = require('../util/middleware')
 
 router.get('/', async (req, res) => {
+  const where = {}
+
+  if (req.query.search) {
+    where.title = {
+      [Op.iLike]: `%${req.query.search}%`
+    }
+  }
+
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
     include: {
       model: User,
       attributes: ['name']
-    }
+    },
+    where
   })
   console.log(JSON.stringify(blogs, null, 2))
   res.json(blogs)
