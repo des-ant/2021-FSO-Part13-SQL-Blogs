a6ea3b0e86caded804b2aae898dfefb535e92623
des-ant <57375593+des-ant@users.noreply.github.com>
2022-04-10 11:50:45 +1000

    Implement functionality in the application to mark a blog in the reading list as read. -ex13.22

--- controllers/readingLists.js
+++ controllers/readingLists.js
@@ -1,19 +1,28 @@
 const router = require('express').Router()
 
 const { ReadingLists } = require('../models')
-// const { tokenExtractor } = require('../util/middleware')
+const { tokenExtractor } = require('../util/middleware')
 
 router.post('/', async (req, res) => {
   const readings = await ReadingLists.create(req.body)
   res.json(readings)
 })
 
-// router.get('/:id', tokenExtractor, async (req, res) => {
-//   const userId = req.decodedToken.id
-//   const blogId = req.params.id
-//   const isRead = req.body.isRead
-//   const readings = await ReadingLists.create({ ...req.body, userId: user.id })
-//   res.json(blog)
-// })
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const reading = await ReadingLists.findOne({
+    where: {
+      userId: req.decodedToken.id,
+      blogId: req.params.id
+    }
+  })
+  console.log(reading)
+  if (reading) {
+    reading.read = req.body.read
+    await reading.save()
+    res.json(reading)
+  } else {
+    res.status(404).end()
+  }
+})
 
 module.exports = router
