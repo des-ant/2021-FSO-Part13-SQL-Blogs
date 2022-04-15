98fabb43bc255cf8a2ad62b497329c2d1b6d9e9a
des-ant <57375593+des-ant@users.noreply.github.com>
2022-04-15 17:36:05 +1000

    Configure connect-session-sequelize

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express()
 require('express-async-errors')
 const session = require('express-session')
 
+// initalize sequelize with session store
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 const { PORT, SESSION_SECRET } = require('./util/config')
-const { connectToDatabase } = require('./util/db')
+const { connectToDatabase, sequelize } = require('./util/db')
 const middleware = require('./util/middleware')
 
 const blogsRouter = require('./controllers/blogs')
@@ -15,7 +18,22 @@ const readingListsRouter = require('./controllers/readingLists')
 
 app.use(express.json())
 
-app.use(session({ secret: SESSION_SECRET }))
+const sessionStore = new SequelizeStore({
+  db: sequelize,
+  checkExpirationInterval: 15 * 60 * 1000, // The interval at which to cleanup expired sessions in milliseconds.
+  expiration: 24 * 60 * 60 * 1000  // The maximum age (in milliseconds) of a valid session.
+})
+
+const sessionConfig = {
+  name: 'session',
+  secret: SESSION_SECRET,
+  store: sessionStore,
+  resave: false, // we support the touch method so per the express-session docs this should be set to false
+  proxy: true, // if you do SSL outside of node.
+  saveUninitialized: true,
+};
+
+app.use(session(sessionConfig))
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
